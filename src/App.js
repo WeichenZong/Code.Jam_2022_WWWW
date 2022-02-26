310bccec4bae38182366568684e7478bae9b6943
Weichen Zong <zeph0528@gmail.com>
2022-02-26 06:32:50 -0500

    react app created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+function App() {
+  return (
+    <Button>Test
+    </Button>
+  );
+}
+
+export default App;
